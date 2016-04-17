b762dcd6d5dfd7126ba438cb1d438bc98507a3d6
iorena <essi.salmenkivi@helsinki.fi>
2016-04-17 22:38:14 +0300

    simple popup when hovering over username

---
+++ who-is.js
@@ -0,0 +1,23 @@
+var popup = document.createElement("div");
+popup.innerHTML = "<h1>moi</h1>";
+popup.setAttribute("class", "redditor-info");
+popup.setAttribute("id", "redditor-info");
+document.body.appendChild(popup);
+
+document.addEventListener("mousemove", positionPopup);
+
+function positionPopup(event) {
+    const srcElement = event.target;
+    if (srcElement.nodeName == "A" && srcElement.hasAttribute("href") && srcElement.getAttribute("href").indexOf("reddit.com/user/") != -1) {
+
+        const x = event.screenX;
+        const y = event.screenY;
+        
+        document.getElementById("redditor-info").style.left = x + "px";
+        document.getElementById("redditor-info").style.top = y + "px";
+        document.getElementById("redditor-info").style.visibility = "visible";
+    } else {
+        document.getElementById("redditor-info").style.visibility = "hidden";
+    }
+}
+
