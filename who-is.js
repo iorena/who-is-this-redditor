33fd737d7ae0c9a858d9e02dc4ced62469151d28
iorena <essi.salmenkivi@helsinki.fi>
2016-04-23 21:09:24 +0300

    fix positioning of popup

--- who-is.js
+++ who-is.js
@@ -8,13 +8,14 @@ document.addEventListener("mousemove", positionPopup);
 
 function positionPopup(event) {
     const srcElement = event.target;
-    if (srcElement.nodeName == "A" && srcElement.hasAttribute("href") && srcElement.getAttribute("href").indexOf("reddit.com/user/") != -1) {
 
+    /* Look for string "reddit.com/user" in links */
+    if (srcElement.nodeName == "A" && srcElement.hasAttribute("href") && srcElement.getAttribute("href").indexOf("reddit.com/user/") != -1) {
         const x = event.screenX;
         const y = event.screenY;
         
         document.getElementById("redditor-info").style.left = x + "px";
-        document.getElementById("redditor-info").style.top = y + "px";
+        document.getElementById("redditor-info").style.top = (srcElement.getBoundingClientRect().top + document.body.scrollTop) + "px";
         document.getElementById("redditor-info").style.visibility = "visible";
     } else {
         document.getElementById("redditor-info").style.visibility = "hidden";
